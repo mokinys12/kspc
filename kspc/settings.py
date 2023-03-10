0b0079d6069daa0f5ad1dccd0b85496768e0c6cd
main <mymail@gmail.com>
2023-03-10 13:39:07 +0200

    2 new sites

--- kspc/settings.py
+++ kspc/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'q$43+6*nos=q5h#xps65ws@37kc)ynumre^bzq=v+5$a*!qwc7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com', '.alwaysdata.com']
 
 
 # Application definition
